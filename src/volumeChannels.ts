95b78e237ccafe1b15a19ba7529d1f9690d634a3
Evan Norton <evan@retro-mmo.com>
2024-04-17 02:57:35 -0400

    Remove boilerplate and add SFX audio channel

--- src/volumeChannels.ts
+++ src/volumeChannels.ts
@@ -1,5 +1,8 @@
 import { createVolumeChannel } from "pixel-pigeon";
 
+export const sfxVolumeChannelID: string = createVolumeChannel({
+  name: "SFX",
+});
 export const musicVolumeChannelID: string = createVolumeChannel({
   name: "Music",
 });
