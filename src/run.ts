6e89529a0bcde8b1f1c909dd9c05f499ad0f5cc9
Evan Norton <evan@retro-mmo.com>
2024-04-21 03:26:38 -0400

    Add catch

--- src/run.ts
+++ src/run.ts
@@ -6,7 +6,9 @@ import { onWindowMessage } from "pixel-pigeon";
 
 export const run = (): void => {
   loadServerURL();
-  loadGameData();
+  loadGameData().catch((error: unknown): void => {
+    throw error;
+  });
   onWindowMessage(handleWindowMessage);
   createUI();
 };
