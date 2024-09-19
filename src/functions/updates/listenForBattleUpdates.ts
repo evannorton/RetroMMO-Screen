3c19dd325dd06fe41f3437779d40a4bd1836a1a2
Evan Norton <evan@retro-mmo.com>
2024-09-19 04:26:12 -0500

    Fix "battle/exit-to-world"

--- src/functions/updates/listenForBattleUpdates.ts
+++ src/functions/updates/listenForBattleUpdates.ts
@@ -1,13 +1,10 @@
 import { BattleExitToWorldUpdate } from "retrommo-types";
-import { WorldCharacter } from "../../classes/WorldCharacter";
 import { createWorldState } from "../state/createWorldState";
-import { getDefinable } from "../../definables";
 import { listenToSocketioEvent } from "pixel-pigeon";
 import { loadWorldCharacterUpdate } from "../loadWorldCharacterUpdate";
 import { loadWorldPartyUpdate } from "../loadWorldPartyUpdate";
 import { selectWorldCharacter } from "../selectWorldCharacter";
 import { state } from "../../state";
-import { updateWorldCharacterPosition } from "../updateWorldCharacterPosition";
 
 export const listenForBattleUpdates = (): void => {
   listenToSocketioEvent<BattleExitToWorldUpdate>({
@@ -17,12 +14,6 @@ export const listenForBattleUpdates = (): void => {
         battleState: null,
         worldState: createWorldState(update.worldCharacterID),
       });
-      const worldCharacter: WorldCharacter = getDefinable(
-        WorldCharacter,
-        update.worldCharacterID,
-      );
-      worldCharacter.tilemapID = update.tilemapID;
-      updateWorldCharacterPosition(worldCharacter.id, update.x, update.y);
       for (const characterUpdate of update.worldCharacters) {
         loadWorldCharacterUpdate(characterUpdate);
       }
