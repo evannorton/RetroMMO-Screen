8349ad9508186ac799bec026501c8f83fad43793
Evan Norton <evan@retro-mmo.com>
2024-10-02 21:54:29 -0500

    Add random colors for players

--- src/functions/addCharacterToWorld.ts
+++ src/functions/addCharacterToWorld.ts
@@ -1,6 +1,6 @@
 import { Color } from "retrommo-types";
 import { WorldCharacter } from "../classes/WorldCharacter";
-import { createEntity, createQuadrilateral } from "pixel-pigeon";
+import { createButton, createEntity, createQuadrilateral } from "pixel-pigeon";
 import { getConstants } from "./getConstants";
 import { getDefinable } from "../definables";
 
@@ -11,6 +11,17 @@ export const addCharacterToWorld = (characterID: string): void => {
   );
   const tileSize: number = getConstants()["tile-size"];
   worldCharacter.entityID = createEntity({
+    buttons: [
+      {
+        buttonID: createButton({
+          height: tileSize,
+          width: tileSize,
+        }),
+        onClick: (): void => {
+          console.log("TODO: handle character click");
+        },
+      },
+    ],
     height: tileSize,
     layerID: "characters",
     levelID: worldCharacter.tilemapID,
@@ -21,7 +32,9 @@ export const addCharacterToWorld = (characterID: string): void => {
     quadrilaterals: [
       {
         quadrilateralID: createQuadrilateral({
-          color: Color.White,
+          color: Object.values(Color)[
+            Math.floor(Math.random() * Object.keys(Color).length)
+          ] as Color,
           height: tileSize,
           width: tileSize,
         }),
