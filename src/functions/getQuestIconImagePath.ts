99fedba657d17d6fc11413ad65a0ac7021f4791e
Evan Norton <evan@retro-mmo.com>
2024-12-15 22:59:35 -0600

    Add quest complete icon

--- src/functions/getQuestIconImagePath.ts
+++ src/functions/getQuestIconImagePath.ts
@@ -1,8 +1,14 @@
 import { Quest } from "../classes/Quest";
+import { QuestState } from "../types/QuestState";
 import { getDefinable } from "definables";
+import { getQuestPartyState } from "./getQuestPartyState";
 
 export const getQuestIconImagePath = (questID: string): string => {
   const quest: Quest = getDefinable(Quest, questID);
+  const state: QuestState | null = getQuestPartyState(questID);
+  if (state === QuestState.Complete) {
+    return "quest-icons/complete";
+  }
   if (quest.hasMonster()) {
     return "quest-icons/monsters";
   }
